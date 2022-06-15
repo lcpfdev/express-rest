b7394d95f3b013e19ee061c44025052fc9fd725e
Luka <lukafrance@gmail.com>
2022-06-15 15:54:42 +0100

    create username

---
+++ src/db/connection.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+// console.log(process.env)
+const mongoose = require("mongoose");
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Succesfully connected");
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+connection()
---
+++ src/user/routes.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+
+const userRouter = Router()
+
+const { createUser } = require("./controllers")
+
+userRouter.post("/user", createUser)
+
+module.exports = userRouter;
