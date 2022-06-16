df15177469ce3f6bbaace439cc4833a6ff60a0c0
Luka <lukafrance@gmail.com>
2022-06-16 16:36:54 +0100

    added get

--- src/user/controllers.js
+++ src/user/controllers.js
@@ -1,16 +1,24 @@
 const User = require("./model")
 
-exports.createUser = async (req, res) => { 
+exports.createUser = async (req, res) => { // Request & Response
  try {
    const userObj = {
       username: req.body.username, 
       email: req.body.email,
       password: req.body.password,
    }
-   const newUser = await User.create(userObj);
-    res.send({ newUser})
- } catch (error) {
+   const newUser = await User.create(userObj); // Adds new user .create is a mongoose method
+    res.send({ newUser })
+   
+ } 
+ catch (error) {
     console.log(error)
     res.send({error})
     } 
 };
+
+exports.getUsers = async (req, res) => {
+   const users = await User.find() //.find() is a mongoose method, mongoose = odm
+   console.log(users)
+   res.send(users) // Returning back to isomnia
+}
--- src/user/routes.js
+++ src/user/routes.js
@@ -4,6 +4,12 @@ const userRouter = Router()
 
 const { createUser } = require("./controllers")
 
+const { getUsers } = require("./controllers")
+ 
 userRouter.post("/user", createUser)
+
+userRouter.get("/user", getUsers)
+
+
 
 module.exports = userRouter;
